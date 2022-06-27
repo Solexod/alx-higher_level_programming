366097c32ebeec3229fe32315fd3557e480102c4
Solexod <101405862+Solexod@users.noreply.github.com>
2022-06-27 16:06:45 -0700

    Update 102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,24 +1,2 @@
-#!/usr/bin/python3
 def magic_calculation(a, b):
-return (98 + a ** b)
-
-
-lists.h
-
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdlib.h>
-
-typedef struct listint_s
-{
-int n;
-struct listint_s *next;
-} listint_t;
-
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
-
-#endif /* LISTS_H */
+    return (98 + a ** b)
