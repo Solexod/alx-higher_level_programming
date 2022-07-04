ba623a1dce5bfe6e88f87df3a17211878969e121
Solexod <101405862+Solexod@users.noreply.github.com>
2022-07-04 15:44:00 -0700

    str commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+    for row in matrix:
+        for item in row:
+            if item == row[-1]:
+                print("{:d}".format(item), end="")
+            else:
+                print("{:d}".format(item), end=" ")
+        print('')
